e3d61bcda305272ae2d2ddd4614aaa34d6bd16f0
Hubert B <116441194+hbg00@users.noreply.github.com>
2024-06-10 15:25:39 +0200

    Add files via upload

--- lab-1/lab1.js
+++ lab-1/lab1.js
@@ -1,33 +1,28 @@
 let checkValue = false;
 
-const button = document.getElementById("buttonResult");
-button.addEventListener("click", fillArray);
-
 const buttonAdd = document.getElementById("addButton");
 buttonAdd.addEventListener("click", addButton);
 
 const buttonsContainer = document.getElementById("buttons");
-buttonsContainer.addEventListener("click", function (event) 
-{
-    if (event.target.classList.contains("delete")) 
-    { 
+buttonsContainer.addEventListener("click", function (event) {
+    if (event.target.classList.contains("delete")) { 
         remove(event);
-        onChange()
+        onChange();
     }
 });
 
-buttonsContainer.addEventListener("change", onChange);
+buttonsContainer.addEventListener("input", onChange);
 
 const addInfo = document.createElement("p");
 const avgInfo = document.createElement("p");
 const minInfo = document.createElement("p");
 const maxInfo = document.createElement("p");
 
-function addButton() 
-{
+function addButton() {
     let div = document.createElement("div");
     let input = document.createElement("input");
     input.classList.add("e");
+    input.type = "number";
     let button = document.createElement("button");
     button.classList.add("delete");
     button.textContent = "Delete";
@@ -35,94 +30,75 @@ function addButton()
     div.append(input, button);
 }
 
-function remove(event)
-{
+function remove(event) {
     const button = event.target;
     const div = button.parentNode;
     div.remove();
 }
 
-function add(parameter)
-{
-    resultSum = 0;
-    for(let i = 0; i < parameter.length; i++)
-    {
+function add(parameter) {
+    let resultSum = 0;
+    for (let i = 0; i < parameter.length; i++) {
         resultSum += parseInt(parameter[i]);
     }
     return resultSum;
 }
 
-function avg(parameter)
-{
-   let sum = add(parameter);
-   return sum / parameter.length;
+function avg(parameter) {
+    let sum = add(parameter);
+    return sum / parameter.length;
 }
 
-function min(parameter)
-{
-    let min = parameter[0];
-    for(let i = 0; i < parameter.length; i++)
-    {
-        if(min > parameter[i])
-        {
-            min = parameter[i];
+function min(parameter) {
+    let minValue = parameter[0];
+    for (let i = 0; i < parameter.length; i++) {
+        if (minValue > parameter[i]) {
+            minValue = parameter[i];
         }
     }
-
-    return min;
+    return minValue;
 }
 
-function max(parameter)
-{
-    let max = 0;
-    for(let i = 0; i < parameter.length; i++)
-    {
-        if(max < parameter[i])
-        {
-            max = parameter[i];
+function max(parameter) {
+    let maxValue = 0;
+    for (let i = 0; i < parameter.length; i++) {
+        if (maxValue < parameter[i]) {
+            maxValue = parameter[i];
         }
     }
-
-    return max;
+    return maxValue;
 }
 
-function onChange()
-{
-    if(checkValue)
-    {
-        let array = []
-        let querySel = document.querySelectorAll(".e");
-        for(let i = 0; i < querySel.length; i++)
-        {
-            array.push(querySel[i].value);
-        }
-           
-        addInfo.textContent = "Sum "+ add(array);
-        avgInfo.textContent = "Avg "+ avg(array);
-        minInfo.textContent = "Min "+ min(array);
-        maxInfo.textContent = "Max "+ max(array);
-            
-        document.getElementById("result").textContent = "";
-        document.getElementById("result").append(addInfo,avgInfo,minInfo,maxInfo);
-    }   
-}
-
-function fillArray()
-{
-    let array = []
+function onChange() {
+    let array = [];
     let querySel = document.querySelectorAll(".e");
-
-    for(let i = 0; i < querySel.length; i++)
-    {
+    for (let i = 0; i < querySel.length; i++) {
         array.push(querySel[i].value);
     }
 
-    addInfo.textContent = "Sum "+ add(array);
-    avgInfo.textContent = "Avg "+ avg(array);
-    minInfo.textContent = "Min "+ min(array);
-    maxInfo.textContent = "Max "+ max(array);
-    
+    addInfo.textContent = "Sum: " + add(array);
+    avgInfo.textContent = "Avg: " + avg(array);
+    minInfo.textContent = "Min: " + min(array);
+    maxInfo.textContent = "Max: " + max(array);
+
     document.getElementById("result").textContent = "";
-    document.getElementById("result").append(addInfo,avgInfo,minInfo,maxInfo);
+    document.getElementById("result").append(addInfo, avgInfo, minInfo, maxInfo);
+}
+
+function fillArray() {
+    let array = [];
+    let querySel = document.querySelectorAll(".e");
+
+    for (let i = 0; i < querySel.length; i++) {
+        array.push(querySel[i].value);
+    }
+
+    addInfo.textContent = "Sum: " + add(array);
+    avgInfo.textContent = "Avg: " + avg(array);
+    minInfo.textContent = "Min: " + min(array);
+    maxInfo.textContent = "Max: " + max(array);
+
+    document.getElementById("result").textContent = "";
+    document.getElementById("result").append(addInfo, avgInfo, minInfo, maxInfo);
     checkValue = true;
 }
